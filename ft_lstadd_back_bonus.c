473f28dd068eabe53015fecaa6fb5eafe51bbfb4
Alireza Sohrabizadeh <asohrabi@c2r2p7.hive.fi>
2023-11-03 16:42:35 +0200

    adding for the 3rd time

---
+++ ft_lstadd_back_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asohrabi <asohrabi@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 16:18:31 by asohrabi          #+#    #+#             */
+/*   Updated: 2023/11/01 12:13:59 by asohrabi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (!new || !lst)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	else
+		(ft_lstlast(*lst))->next = new;
+}
